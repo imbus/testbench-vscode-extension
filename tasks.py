e31f07e5556e633ce1df7e98ff1e7ddb03b47199
Henrik Schütte <henriksch@imbus.de>
2025-04-29 11:22:50 +0200

    update build task

--- tasks.py
+++ tasks.py
@@ -1,7 +1,9 @@
 from invoke import task
+import shutil
 
 @task
 def build_vsix(c):
+    shutil.rmtree("bundled/libs")
     c.run("pip-compile")
     c.run("python -m nox --session bundle_dependencies")
     c.run("npm run vsix-package")
