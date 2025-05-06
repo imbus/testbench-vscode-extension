bc305031ed85e5d387c5ff879b637187eb317498
HenrikSchuette <Henrik.schuette@gmx.de>
2025-05-06 08:11:58 +0200

    local development

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,17 @@ import * as path from "path";
 export const EXTENSION_ID = "imbus.testbench-visual-studio-code-extension";
 export const EXTENSION_ROOT_DIR = path.dirname(__dirname);
 export const BUNDLED_PYTHON_SCRIPTS_DIR = path.join(EXTENSION_ROOT_DIR, "bundled");
-export const LANGUAGE_SERVER_SCRIPT_PATH = path.join(BUNDLED_PYTHON_SCRIPTS_DIR, "libs", "testbench_ls", "__main__.py");
+export const LANGUAGE_SERVER_SCRIPT_PATH = path.join(
+    BUNDLED_PYTHON_SCRIPTS_DIR,
+    "libs",
+    "testbench_ls",
+    "language_server.py"
+);
+
+export const LANGUAGE_SERVER_DEBUG_PATH = path.join(
+    EXTENSION_ROOT_DIR,
+    "packages",
+    "testbench-ls",
+    "testbench_ls",
+    "language_server_debug.py"
+);
