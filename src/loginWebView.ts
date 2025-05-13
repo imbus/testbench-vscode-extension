f7a974a0e25d1960e5f7949f4dc15b8d09df3974
egegirit <egegirit@gmail.com>
2025-05-13 11:51:04 +0200

    feat: Add Content Security Policy to login webview

--- src/loginWebView.ts
+++ src/loginWebView.ts
@@ -252,11 +252,16 @@ export class LoginWebViewProvider implements vscode.WebviewViewProvider {
             : "";
         const autoLoginChecked: string = getConfig().get<boolean>(ConfigKeys.AUTO_LOGIN, false) ? "checked" : "";
 
+        // Define a Content Security Policy
+        // ${webview.cspSource} is a placeholder for VS Code's webview resource sources.
+        const contentSecurityPolicy: string = `default-src 'none'; img-src ${webview.cspSource} https: data:; script-src 'unsafe-inline' ${webview.cspSource}; style-src ${webview.cspSource} 'unsafe-inline'; font-src ${webview.cspSource};`;
+
         return `
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8"/>
+    <meta http-equiv="Content-Security-Policy" content="${contentSecurityPolicy}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login to TestBench</title>
     <style>
