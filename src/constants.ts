7b74ea19c1954bd58eb9c214009b70d8e74896a5
Ege Girit <egeg@imbus.de>
2025-07-11 11:42:32 +0200

    feat(config): add test explorer auto-open setting, update config categories

--- src/constants.ts
+++ src/constants.ts
@@ -67,7 +67,8 @@ export const ConfigKeys = {
     TB2ROBOT_RESOURCE_ROOT: "resourceRoot",
     TB2ROBOT_LIBRARY_MAPPING: "libraryMapping",
     TB2ROBOT_RESOURCE_MAPPING: "resourceMapping",
-    TB2ROBOT_OUTPUT_XML_PATH: "outputXmlFilePath"
+    TB2ROBOT_OUTPUT_XML_PATH: "outputXmlFilePath",
+    OPEN_TEST_EXPLORER_AFTER_GENERATION: "openTestExplorerAfterGeneration"
 } as const;
 
 // --- Context Keys ---
