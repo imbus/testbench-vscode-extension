0962d42069e03df90f0be7c16199098962552fa0
egegirit <egegirit@gmail.com>
2025-09-05 14:41:27 +0200

    feat(workflows): enhance manual release inputs and add Python setup in release workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    extends: ["@commitlint/config-conventional"]
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "header-max-length": [2, "always", 150] // Increase character limit of commits from 100 (default) to 150
+    }
 };
